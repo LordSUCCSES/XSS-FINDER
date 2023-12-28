a6167b6d5ab9dc164dd8021f8d64ca7b1daacac2
LordSUCCSES <145927861+LordSUCCSES@users.noreply.github.com>
2023-12-28 21:00:45 +0300

    update

--- xssfind.py
+++ xssfind.py
@@ -1,6 +1,15 @@
 import requests
 import re
 
+class Color:
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BROWN = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+
 art = '''\
  ____  ___  _________ _________ ___________.___ _______  ________  _____________________ 
 \   \/  / /   _____//   _____/ \_   _____/|   |\      \ \______ \ \_   _____/\______   \\
@@ -9,7 +18,7 @@ art = '''\
 /___/\  \/_______  /_______  /  \___  /   |___\____|__  /_______  /_______  / |____|_  /
       \_/        \/        \/       \/                \/        \/        \/         \/ '''
 
-print(art)
+print(Color.BROWN + art + Color.GREEN)
 
 url = input('Url Ve Payload Girin (Örnek: https://www.deneme.com/deneme.php?param=<script>alert(""mera"");</script>): ')
 
@@ -21,10 +30,10 @@ if response.status_code == 200:
     aranan_kelime = 'alert'
 
     if re.search(aranan_kelime, content, re.IGNORECASE):
-        print(f"{url} Url'sinde XSS Zafiyeti Mevcut!")
+        print(Color.BLUE + f"{url} Url'sinde XSS Zafiyeti Mevcut!")
         with open("bulundu.txt" ,"w") as dosya:
             dosya.write(url)
     else:
-        print(f"{url} Url'sinde XSS Zafiyeti Mevcut Değil")
+        print(Color.RED + f"{url} Url'sinde XSS Zafiyeti Mevcut Değil")
 else:
-    print(f"URL'ye erişilemedi")
+    print(Color.RED + f"URL'ye erişilemedi")
